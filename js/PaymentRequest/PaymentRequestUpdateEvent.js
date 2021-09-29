19f39b5da91000c44ec044eee4c811c02345f9dc
Caroline Shea <caroline@portalmedia.com>
2021-09-29 16:56:30 -0400

    Revert "remove workaround for an ios specific bug that no longer seems to exist"
    This reverts commit 40148edf4f72e9031d6f5b9006491f51d008350d.

--- js/PaymentRequest/PaymentRequestUpdateEvent.js
+++ js/PaymentRequest/PaymentRequestUpdateEvent.js
@@ -117,9 +117,29 @@ export default class PaymentRequestUpdateEvent {
       NativePayments.handleDetailsUpdate(normalizedDetails, DOMException)
         // 14. Upon fulfillment of detailsPromise with value value
         .then(this._resetEvent())
+        // On iOS the `selectedShippingMethod` defaults back to the first option
+        // when updating shippingMethods.  So we call the `_handleShippingOptionChange`
+        // method with the first shippingOption id so that JS is in sync with Apple Pay.
+        .then(() => {
+          if (Platform.OS !== 'ios') {
+            return;
+          }
+
+          if (
+            target._details.shippingOptions
+            && target._details.shippingOptions.length > 0
+            && value.shippingOptions
+            && ((value.shippingOptions.find(op => op.selected) || {}).id || null) !== target._shippingOption
+          ) {
+            target._handleShippingOptionChange({
+              selectedShippingOptionId: target._details.shippingOptions[0].id
+            });
+          }
+        })
         // 13. Upon rejection of detailsPromise:
         .catch(e => {
           this._resetEvent();
+
           throw new Error('AbortError');
         })
     );
